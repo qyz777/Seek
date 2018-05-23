766d1fdb220b50f3a9306cde1f85281fb2328a06
Q YiZhong <178159283@qq.com>
2018-05-23 16:05:08 +0800

    完成单词接口

--- Seek/model/word/YZWord.h
+++ Seek/model/word/YZWord.h
@@ -12,16 +12,28 @@
 
 @property(nonatomic, strong)NSString *word;
 @property(nonatomic, strong)NSString *sentence;
+@property(nonatomic, strong)NSString *senTranslate;
 @property(nonatomic, strong)NSString *ukPhone;
 @property(nonatomic, strong)NSString *usPhone;
 @property(nonatomic, strong)NSURL *ukPhoneUrl;
 @property(nonatomic, strong)NSURL *usPhoneUrl;
 @property(nonatomic, strong)NSArray *translate;
+@property(nonatomic, assign)BOOL isLiked;
 
 
 // 搜索页搜索单词使用
 + (void)searchWordWithString:(NSString *)str
                      success:(void(^)(NSArray<NSDictionary *> *dataArray))success
                      failure:(void(^)(NSError *error))failure;
+
+// 根据单词搜索单词详情
++ (void)searchDetailsWithWord:(NSString *)word
+                      success:(void(^)(YZWord *yzWord))success
+                      failure:(void(^)(NSError *error))failure;
+
+// 添加或删除单词
++ (void)likeWithWord:(NSString *)word
+             success:(void(^)(BOOL isLike))success
+             failure:(void(^)(NSError *error))failure;
 
 @end
--- Seek/model/word/YZWord.m
+++ Seek/model/word/YZWord.m
@@ -30,5 +30,66 @@
     }];
 }
 
++ (void)searchDetailsWithWord:(NSString *)word
+                      success:(void(^)(YZWord *yzWord))success
+                      failure:(void(^)(NSError *error))failure {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    NSString *url = @"http://seek-api.xuzhengke.cn/index.php/Api/Word/wordDetail";
+    NSDictionary *parameters = @{@"word": word, @"user_id": @([User sharedUser].userId)};
+    [manager GET:url parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSDictionary *json = responseObject;
+        if (json) {
+            int code = [json[@"code"]intValue];
+            if (code == 0) {
+                YZWord *dataWord = [YZWord new];
+                dataWord.word = json[@"word"];
+                dataWord.sentence = json[@"sen"];
+                dataWord.senTranslate = json[@"sen_tran"];
+                NSDictionary *speech = json[@"speech"];
+                NSDictionary *uk = speech[@"uk"];
+                NSDictionary *us = speech[@"us"];
+                dataWord.ukPhone = uk[@"phone"];
+                dataWord.ukPhoneUrl = uk[@"speech"];
+                dataWord.usPhone = us[@"phone"];
+                dataWord.usPhoneUrl = us[@"speech"];
+                dataWord.translate = json[@"trans"];
+                if ([json[@"like"]intValue] == 0) {
+                    dataWord.isLiked = false;
+                }else {
+                    dataWord.isLiked = true;
+                }
+                success(dataWord);
+            }
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        failure(error);
+    }];
+}
+
++ (void)likeWithWord:(NSString *)word
+             success:(void(^)(BOOL isLike))success
+             failure:(void(^)(NSError *error))failure {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    NSString *url = @"http://seek-api.xuzhengke.cn/index.php/Api/Word/likeWord";
+    NSDictionary *parameters = @{@"user_id": @([User sharedUser].userId),@"token": [User sharedUser].token,@"word": word};
+    [manager POST:url parameters:parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSDictionary *json = responseObject;
+        if (json) {
+            int code = [json[@"code"] intValue];
+            if (code == 1) {
+                success(false);
+            }
+            if (code == 0) {
+                success(true);
+            }
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        failure(error);
+    }];
+}
 
 @end
